1df4468ca53e06c6dfb2ae4ff830257a6df75175
Ryan Bogle <ryan.bogle@gmail.com>
2018-04-24 15:46:22 -0700

    feat: update drinks for 2018-04

--- client/cards/drinks.js
+++ client/cards/drinks.js
@@ -4,6 +4,6 @@ module.exports = h('.drinks',
   h('h1', 'Drinks.js @ 9:15pm'),
 
   h('img', {src: '/img/drinks/logo.png'}),
-  h('h1', 'Big Bar at the Alcove Cafe'),
-  h('h2', '1927 Hillhurst Ave (Hillhurst & Franklin)')
+  h('h1', 'The Golden Gopher'),
+  h('h2', '417 W 8th St (between Olive and Hill)')
 )
