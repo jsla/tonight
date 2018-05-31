6006205fcf97aad11780f18df09887624ff072d0
Ryan Bogle <ryan.bogle@gmail.com>
2018-05-31 11:40:36 -0700

    feat: update drinks for 2018-05

--- client/cards/drinks.js
+++ client/cards/drinks.js
@@ -4,6 +4,6 @@ module.exports = h('.drinks',
   h('h1', 'Drinks.js @ 9:15pm'),
 
   h('img', {src: '/img/drinks/logo.png'}),
-  h('h1', 'The Golden Gopher'),
-  h('h2', '417 W 8th St (between Olive and Hill)')
+  h('h1', 'The Daily Pint'),
+  h('h2', '2310 Pico Blvd (between Cloverfield and 23rd)')
 )
