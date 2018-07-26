99c7bd6ba61eb3c6f4468f9c54a6102f9e5ba770
Ryan Bogle <ryan.bogle@gmail.com>
2018-07-26 11:57:10 -0700

    feat: update drinks for July 2018

--- client/cards/drinks.js
+++ client/cards/drinks.js
@@ -4,6 +4,7 @@ module.exports = h('.drinks',
   h('h1', 'Drinks.js @ 9:15pm'),
 
   h('img', {src: '/img/drinks/logo.png'}),
-  h('h1', 'Golden Gopher'),
-  h('h2', '417 W 8th St (between Olive and Hill)')
+  h('h1', 'Alley Lounge'),
+  h('h2', '12223 W Washington Blvd, Culver City'),
+  h('h2', 'Just east of Centinela')
 )
