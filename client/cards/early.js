6bb932f662c929f80a5b94b9fbea41b789714aef
Rouzbeh Sarrafieh <rouzbeh.sarrafieh@gmail.com>
2018-07-26 19:27:58 -0700

    add next rsvp card

---
+++ client/cards/early.js
@@ -0,0 +1,14 @@
+var h = require('hyperscript')
+
+module.exports = h('.coc',
+  h('h1', 'Limited to you, only tonight,'),
+
+  h('h1', 'get a ticket to the next js.la first before anyone else can!'),
+
+  h('h1',
+    h('span.highlight', 'https://js.la/next')
+  ),
+
+  h('h1', 'Tickets are ONLY available during tonight\'s event!')
+
+)
