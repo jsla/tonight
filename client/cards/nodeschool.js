c884fe126caf7e9cdd063295aa9d7069db09c50c
David Guttman <david@davidguttman.com>
2019-03-28 19:14:45 -0700

    fix: nodeschool iframe fix

--- client/cards/nodeschool.js
+++ client/cards/nodeschool.js
@@ -2,11 +2,11 @@ var h = require('hyperscript')
 
 module.exports = h('.iframe',
   h('iframe', {
-    src: 'https://nodeschool.io/los-angeles',
+    src: 'https://nodeschool.io/los-angeles/',
     scrolling: 'no',
     style: {
       height: '86%'
     }
   }),
-  h('h1', 'nodeschool.io/los-angeles')
+  h('h1', 'js.la/nodeschool')
 )
