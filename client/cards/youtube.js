2e4a5bdaeef33738faeca1cbee2932a885c7c22f
David Guttman <david@davidguttman.com>
2019-03-28 19:21:52 -0700

    fix: youtube height fix

--- client/cards/youtube.js
+++ client/cards/youtube.js
@@ -1,7 +1,13 @@
 var h = require('hyperscript')
 
 module.exports = h('.youtube',
-  h('img.frame', {src: 'https://i.imgur.com/VYtsULv.jpg', style: {width: '80%'}}),
+  h('img.frame', {src: 'https://i.imgur.com/VYtsULv.jpg',
+    style: {
+      width: '80%',
+      color: 'red',
+      'max-height': '90vh'
+    }
+  }),
 
   h('h1', 'js.la/youtube')
 )
