73a91cc6cc0acbdf3621582dc616d5653e892702
David Guttman <david@davidguttman.com>
2019-06-26 23:17:35 -0700

    update events

--- script/update.js
+++ script/update.js
@@ -10,7 +10,7 @@ fetchAdmin(function (err, everything) {
   }
 
   var current = extractShow(everything, Date.now() - (7 * 24 * 3600 * 1000))
-  var next = extractShow(everything, Date.now())
+  var next = extractShow(everything, Date.now() + (7 * 24 * 3600 * 1000))
 
   var fn = path.join(__dirname, '../client/events.json')
   fs.writeFile(fn, JSON.stringify({current, next}, null, 2), function () {})
