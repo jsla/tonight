49fe8aab1f2ce3c90010d783f5a109d4966ba9b1
Spencer Allen <spencer.r.allen@gmail.com>
2019-07-25 09:56:06 -0700

    Update picktrace.js

--- client/cards/picktrace.js
+++ client/cards/picktrace.js
@@ -1,7 +1,7 @@
 var h = require('hyperscript')
 
 module.exports = h('.youtube',
-  h('img.frame', {src: '/img/picktrace.png',
+  h('img.frame', {src: '/img/picktrace.jpg',
     style: {
       width: '80%',
       color: 'red',
