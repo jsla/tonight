23105a3c6517f138aa897094504ac4384cbf59f9
David Guttman <david@davidguttman.com>
2019-08-29 19:45:15 -0700

    fix: use working lunch image

--- client/cards/lunch.js
+++ client/cards/lunch.js
@@ -2,7 +2,7 @@ var h = require('hyperscript')
 
 module.exports = h('.youtube',
   h('img.frame', {
-    src: 'https://cl.ly/3702d4589fb6/Image%2525202018-10-25%252520at%2525207.09.29%252520PM.png',
+    src: '/img/lunch.png',
     style: {width: '80%'}
   }),
 
