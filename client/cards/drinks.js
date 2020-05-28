84715a27b813ff4f3f372947555e56b7f7858bf2
David Guttman <david@davidguttman.com>
2020-05-28 13:37:10 -0700

    feat(drinks): talkworld teaser

--- client/cards/drinks.js
+++ client/cards/drinks.js
@@ -1,28 +1,11 @@
 var h = require('hyperscript')
 
-var prefixOpts = { style: { color: 'rgba(255,255,255,0.5)' } }
-
 module.exports = function ({name, address}) {
   return h('.drinks',
     h('h1', 'Drinks.js after the talks'),
 
     h('img', {src: '/img/drinks.svg'}),
-    h('h1', 'Four different venues to choose from:'),
-    h('h2',
-      h('span', prefixOpts, 'https://js.la/zoom'),
-      h('span', '/stage')
-    ),
-    h('h2',
-      h('span', prefixOpts, 'https://js.la/zoom'),
-      h('span', '/bar')
-    ),
-    h('h2',
-      h('span', prefixOpts, 'https://js.la/zoom'),
-      h('span', '/lounge')
-    ),
-    h('h2',
-      h('span', prefixOpts, 'https://js.la/zoom'),
-      h('span', '/patio')
-    )
+    h('h1', 'Join us in a private virtual world'),
+    h('h1', 'Keep an eye on #general in js.la/slack for details!'),
   )
 }
