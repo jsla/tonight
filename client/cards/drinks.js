2b42aab23bb5f1ab4ea4f129a58a1d5e53905d19
David Guttman <david@davidguttman.com>
2020-07-30 17:43:55 -0700

    feat: update drinks to rambly url

--- client/cards/drinks.js
+++ client/cards/drinks.js
@@ -5,7 +5,7 @@ module.exports = function ({name, address}) {
     h('h1', 'Drinks.js after the talks'),
 
     h('img', {src: '/img/drinks.svg'}),
-    h('h1', 'Join us in a private virtual world'),
-    h('h1', 'Keep an eye on #general in js.la/slack for details!'),
+    h('h1', 'Join us in Rambly:'),
+    h('h1', 'https://ramb.ly/twitch/jsdotla'),
   )
 }
